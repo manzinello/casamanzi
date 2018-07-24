f56ef4a87eedf94173b1a5eaad9c378db270e8a1
Matteo Manzinello <matteo@manzinello.com>
2018-07-24 10:13:46 +0200

    Inseriti testi di prova con evergreen-ui

--- client/src/Casamanzi.js
+++ client/src/Casamanzi.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import './Casamanzi.css';
 
-import { Button } from 'evergreen-ui'
+import { Heading, Text, Button } from 'evergreen-ui'
 
 class Casamanzi extends Component {
 
@@ -25,7 +25,13 @@ class Casamanzi extends Component {
     return (
       <div>
 
-        <p>Casamanzi</p>
+        <Heading>casamanzi</Heading>
+
+        <br />
+
+        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque tortor justo, eu viverra nulla placerat id. Vestibulum turpis est, lacinia eget rhoncus ut, ornare at tortor. Donec venenatis vestibulum neque, ac posuere ante pulvinar sit amet. Etiam sed tempus metus, a fringilla sem. Aenean condimentum lorem ac lorem consectetur convallis. Praesent semper mauris neque, vel ultrices mi accumsan a. Aenean lacinia laoreet fringilla.</Text>
+
+        <br /><br />
 
         <Button>I am using 🌲 Evergreen!</Button>
 
