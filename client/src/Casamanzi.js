d2ee8752cd704b52cf0f657fe8b80166674d7596
Matteo Manzinello <matteo@manzinello.com>
2018-07-24 09:59:39 +0200

    Rimosso grommet

--- client/src/Casamanzi.js
+++ client/src/Casamanzi.js
@@ -2,15 +2,6 @@ import React, { Component } from 'react';
 
 import './Casamanzi.css';
 
-import App from 'grommet/components/App';
-import Header from 'grommet/components/Header';
-import Section from 'grommet/components/Section';
-import Paragraph from 'grommet/components/Paragraph'
-import Title from 'grommet/components/Title';
-import Box from 'grommet/components/Box';
-import Footer from 'grommet/components/Footer';
-import Button from 'grommet/components/Button';
-
 class Casamanzi extends Component {
 
   state = {
@@ -32,26 +23,7 @@ class Casamanzi extends Component {
     return (
       <App>
 
-        <Header size='large'>
-          <Title>
-            casamanzi
-          </Title>
-        </Header>
-
-        <Section>
-          <Paragraph size='medium'><strong>lorem</strong> ipsum</Paragraph>
-        </Section>
-
-        <Footer justify='between'
-          size='small'>
-          <Box direction='row'
-            align='center'
-            pad={{ "between": "medium" }}>
-            <Paragraph size='small'>
-              casamanzi v0.1, creata da <a href="https://matteomanzinello.com" target="_blank" rel="noopener noreferrer"><strong>matteo manzinello</strong></a>
-            </Paragraph>
-          </Box>
-        </Footer>
+        <p>Casamanzi</p>
 
       </App>
     );
