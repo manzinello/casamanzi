1b93ef36cda892c252bc1b44db51d1d093653060
Matteo Manzinello <matteo@manzinello.com>
2018-07-24 11:52:11 +0200

    Rimosso evergreen-ui

--- client/src/Casamanzi.js
+++ client/src/Casamanzi.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 
 import './Casamanzi.css';
 
-import { Heading, Text, Button, Code } from 'evergreen-ui'
-
 class Casamanzi extends Component {
 
   state = {
@@ -25,19 +23,7 @@ class Casamanzi extends Component {
     return (
       <div>
 
-        <Heading>casamanzi</Heading>
-
-        <br />
-
-        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque tortor justo, eu viverra nulla placerat id. Vestibulum turpis est, lacinia eget rhoncus ut, ornare at tortor. Donec venenatis vestibulum neque, ac posuere ante pulvinar sit amet. Etiam sed tempus metus, a fringilla sem. Aenean condimentum lorem ac lorem consectetur convallis. Praesent semper mauris neque, vel ultrices mi accumsan a. Aenean lacinia laoreet fringilla.</Text>
-
-        <br /><br />
-
-        <Button>I am using 🌲 Evergreen!</Button>
-
-        <br /><br />
-
-        <Code>casamanzi, fatta con ❤️ da matteo manzinello</Code>
+        <p>casamanzi</p>
 
       </div>
     );
