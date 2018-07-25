f7ff0ddd30f1a9e4862d463eb7b6a7afe8c22fa2
Matteo Manzinello <matteo@manzinello.com>
2018-07-25 14:58:19 +0200

    Prove con ant.design

--- client/src/Casamanzi.js
+++ client/src/Casamanzi.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react';
 
 import './Casamanzi.css';
 
+import { Layout, Menu, Icon } from 'antd';
+
+import Button from 'antd/lib/button';
+
+const { Header, Sider, Content } = Layout;
+
 class Casamanzi extends Component {
 
   state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
   }
 
   async componentDidMount() {
@@ -21,9 +34,41 @@ class Casamanzi extends Component {
 
   render() {
     return (
-      <div>
-        <p>casamanzi</p>
-      </div>
+      <Layout>
+        <Sider
+          trigger={null}
+          collapsible
+          collapsed={this.state.collapsed}
+        >
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              <Icon type="user" />
+              <span>nav 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="video-camera" />
+              <span>nav 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="upload" />
+              <span>nav 3</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            <Icon
+              className="trigger"
+              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={this.toggle}
+            />
+          </Header>
+          <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
+            Content
+          </Content>
+        </Layout>
+      </Layout>
     );
   }
 
