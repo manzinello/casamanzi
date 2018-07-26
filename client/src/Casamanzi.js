138935c8fbf97621cfcacad95f8c1bfc7f21166a
Matteo Manzinello <matteo@manzinello.com>
2018-07-26 14:28:54 +0200

    Bozza navigazione ant

--- client/src/Casamanzi.js
+++ client/src/Casamanzi.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 
 import './Casamanzi.css';
 
-import { Layout, Menu, Breadcrumb, Button } from 'antd';
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
 
-const { Header, Content, Footer } = Layout;
+const { Header, Content, Footer, Sider } = Layout;
+const SubMenu = Menu.SubMenu;
 
 class Casamanzi extends Component {
 
   state = {
     collapsed: false,
   };
+
+  onCollapse = (collapsed) => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  }
+
 
   toggle = () => {
     this.setState({
@@ -32,35 +39,55 @@ class Casamanzi extends Component {
 
   render() {
     return (
-      <Layout className="layout">
-        <Header>
-          <div className="logo" />
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={['1']}
-            style={{ lineHeight: '64px' }}
-          >
-            <Menu.Item key="1">Benvenuto!</Menu.Item>
-            <Menu.Item key="2">manzistreaming</Menu.Item>
-            <Menu.Item key="3">altro</Menu.Item>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider
+          collapsible
+          collapsed={this.state.collapsed}
+          onCollapse={this.onCollapse}>
+          <div className="logo" style={{color:'white'}}>casamanzi</div>
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1">
+              <Icon type="pie-chart" />
+              <span>Option 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="desktop" />
+              <span>Option 2</span>
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={<span><Icon type="user" /><span>User</span></span>}>
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub2"
+              title={<span><Icon type="team" /><span>Team</span></span>}>
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9">
+              <Icon type="file" />
+              <span>File</span>
+            </Menu.Item>
           </Menu>
-        </Header>
-        <Content style={{ padding: '0 50px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
-            Content
-          <br />
-            <Button type="primary">Primary</Button>
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>
-          casamanzi, creata con ❤️ da <a href="https://matteomanzinello.com">matteo manzinello</a>
-        </Footer>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }} />
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+              Bill is a cat.
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            <strong>casamanzi</strong>, creata da <a href="https://matteomanzinello.com">matteo manzinello</a>
+          </Footer>
+        </Layout>
       </Layout>
     );
   }
@@ -68,4 +95,3 @@ class Casamanzi extends Component {
 }
 
 export default Casamanzi;
-
