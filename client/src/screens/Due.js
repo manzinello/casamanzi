ae0cce0c7b1b7b3c9a4d028e44bc46ab2c24693a
Matteo Manzinello <matteo@manzinello.com>
2018-07-27 17:36:39 +0200

    Bozza Due

--- client/src/screens/Due.js
+++ client/src/screens/Due.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+import { Row, Col } from 'antd';
+
 class Due extends Component {
 
     render() {
         return (
-            <p>Due</p>
+            <Row>
+                <Col xs={1} sm={1} md={1} lg={1} xl={1}>Col</Col>
+                <Col xs={1} sm={1} md={1} lg={1} xl={1}>Col</Col>
+                <Col xs={1} sm={1} md={1} lg={1} xl={1}>Col</Col>
+            </Row>
         )
     }
 
