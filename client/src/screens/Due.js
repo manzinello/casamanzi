88798602d166f96093774d97625a7e4da2e90e26
Matteo Manzinello <matteo@manzinello.com>
2018-09-03 19:04:24 +0200

    Bozza punteggi gioco del 2

--- client/src/screens/Due.js
+++ client/src/screens/Due.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react';
 
-import { Row, Col } from 'antd';
+import { Row, Col, Button } from 'antd';
 
 class Due extends Component {
+
+    state = {
+        g1: 0,
+        g2: 0,
+        g3: 0,
+        g4: 0,
+        g5: 0
+    }
 
     render() {
         return (
             <Row>
-                <Col xs={24} sm={24} md={4} lg={4} xl={4}>Col</Col>
-                <Col xs={24} sm={24} md={4} lg={4} xl={4}>Col</Col>
-                <Col xs={24} sm={24} md={4} lg={4} xl={4}>Col</Col>
-                <Col xs={24} sm={24} md={4} lg={4} xl={4}>Col</Col>
-                <Col xs={24} sm={24} md={4} lg={4} xl={4}>Col</Col>
+                <Col style={styles.col} xs={24} sm={24} md={4} lg={4} xl={4}>
+                    <h3>Giocatore 1</h3>
+                    {this.state.g1}
+                </Col>
+                <Col style={styles.col} xs={24} sm={24} md={4} lg={4} xl={4}>
+                    <h3>Giocatore 2</h3>
+                    {this.state.g2}
+                </Col>
+                <Col style={styles.col} xs={24} sm={24} md={4} lg={4} xl={4}>
+                    <h3>Giocatore 3</h3>
+                    {this.state.g3}
+                </Col>
+                <Col style={styles.col} xs={24} sm={24} md={4} lg={4} xl={4}>
+                    <h3>Giocatore 4</h3>
+                    {this.state.g4}
+                </Col>
+                <Col style={styles.col} xs={24} sm={24} md={4} lg={4} xl={4}>
+                    <h3>Giocatore 5</h3>
+                    {this.state.g5}
+                </Col>
             </Row>
         )
     }
 
+}
+
+const styles = {
+    col: {
+        textAlign: 'center'
+    }
 }
 
 export default Due;
