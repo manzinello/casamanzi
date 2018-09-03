9d008f5ddfe3769657fac8c11a74cf0713816c73
Matteo Manzinello <matteo@manzinello.com>
2018-09-03 19:52:21 +0200

    Bozza funzionamento del gioco

--- client/src/screens/Due.js
+++ client/src/screens/Due.js
@@ -5,35 +5,76 @@ import { Row, Col, Button } from 'antd';
 class Due extends Component {
 
     state = {
-        g1: 0,
-        g2: 0,
-        g3: 0,
-        g4: 0,
-        g5: 0
+        g1: {
+            nome: 'Giocatore 1',
+            punti: 0
+        },
+        g2: {
+            nome: 'Giocatore 2',
+            punti: 0
+        },
+        g3: {
+            nome: 'Giocatore 3',
+            punti: 0
+        },
+        g4: {
+            nome: 'Giocatore 4',
+            punti: 0
+        },
+        g5: {
+            nome: 'Giocatore 5',
+            punti: 0
+        },
+    }
+
+    checkPunti() {
+        return this.state.g1.punti + this.state.g2.punti + this.state.g3.punti + this.state.g4.punti + this.state.g5.punti === 0
+    }
+
+    aggiornaPunti(nome, aggiungi) {
+        for (var giocatore in this.state) {
+            if (nome === this.state[giocatore].nome) {
+                this.state[giocatore].punti += aggiungi
+            }
+        }
+    }
+
+    win(comandante, socio, moltiplicatore = 1) {
+
+        this.aggiornaPunti(comandante, 2 * moltiplicatore);
+        this.aggiornaPunti(socio, 1 * moltiplicatore);
+
+        for (var giocatore in this.state) {
+            if (this.state[giocatore].nome !== comandante && this.state[giocatore].nome !== socio) {
+                this.aggiornaPunti(this.state[giocatore].nome, -1 * moltiplicatore);
+            }
+        }
+
     }
 
     render() {
+
         return (
             <Row>
                 <Col style={styles.col} xs={24} sm={24} md={4} lg={4} xl={4}>
-                    <h3>Giocatore 1</h3>
-                    {this.state.g1}
+                    <h3>{this.state.g1.nome}</h3>
+                    {this.state.g1.punti}
                 </Col>
                 <Col style={styles.col} xs={24} sm={24} md={4} lg={4} xl={4}>
-                    <h3>Giocatore 2</h3>
-                    {this.state.g2}
+                    <h3>{this.state.g2.nome}</h3>
+                    {this.state.g2.punti}
                 </Col>
                 <Col style={styles.col} xs={24} sm={24} md={4} lg={4} xl={4}>
-                    <h3>Giocatore 3</h3>
-                    {this.state.g3}
+                    <h3>{this.state.g3.nome}</h3>
+                    {this.state.g3.punti}
                 </Col>
                 <Col style={styles.col} xs={24} sm={24} md={4} lg={4} xl={4}>
-                    <h3>Giocatore 4</h3>
-                    {this.state.g4}
+                    <h3>{this.state.g4.nome}</h3>
+                    {this.state.g4.punti}
                 </Col>
                 <Col style={styles.col} xs={24} sm={24} md={4} lg={4} xl={4}>
-                    <h3>Giocatore 5</h3>
-                    {this.state.g5}
+                    <h3>{this.state.g5.nome}</h3>
+                    {this.state.g5.punti}
                 </Col>
             </Row>
         )
