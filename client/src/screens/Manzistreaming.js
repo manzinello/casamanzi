e501ad4e0279f66d49961d51182c3f4de728d771
Matteo Manzinello <matteo@manzinello.com>
2018-10-14 15:49:06 +0200

    Bozza per interrogare effettivamente manzistreaming

--- client/src/screens/Manzistreaming.js
+++ client/src/screens/Manzistreaming.js
@@ -1,11 +1,36 @@
 import React, { Component } from "react";
 
 class Manzistreaming extends Component {
+  manzistreamingEndpoint = "";
+
+  state = {
+    active: false
+  };
+
+  componentDidMount = () => {
+    fetch(manzistreamingEndpoint)
+      .then(response => {
+        if (response.statusCode === 200) {
+          this.setState({ active: true });
+        }
+      })
+      .catch(() => {});
+  };
+
   render() {
     return (
       <p>
-        manzi
-        <strong>streaming</strong> non è attivo al momento!
+        {this.state.active ? (
+          <React.Fragment>
+            manzi
+            <strong>streaming</strong> è attivo al momento!
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            manzi
+            <strong>streaming</strong> NON è attivo al momento!
+          </React.Fragment>
+        )}
       </p>
     );
   }
